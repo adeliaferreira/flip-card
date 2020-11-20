61388b174a256a2a892d9e983461229f7c158fcf
adelia ferreira <tay.figueira@gmail.com>
2020-11-20 16:36:02 +0000

    Added rotate on the flip card
    Signed-off-by: adelia ferreira <tay.figueira@gmail.com>

--- cassino-card/card-lottery.js
+++ cassino-card/card-lottery.js
@@ -1,11 +1,14 @@
 function fliperCard(hiddenId, showId) {
- 
+    //get obj from dom tree using getElementById
     const hiddenObj = document.getElementById(hiddenId);
     const showObj = document.getElementById(showId);
-
-    hiddenObj.style.visibility = 'hidden';
-    showObj.style.visibility = 'visible';
- 
-    console.log(hiddenObj);
-    console.log(showObj);
+    //remove previous class to hidden
+    hiddenObj.classList.remove('card-rotate-hidden');
+    showObj.classList.remove('card-rotate-hidden');
+    //remove previous class to show
+    hiddenObj.classList.remove('card-rotate-show');
+    showObj.classList.remove('card-rotate-show');
+    //initial new events
+    hiddenObj.classList.add('card-rotate-hidden');
+    showObj.classList.add('card-rotate-show');
 }
