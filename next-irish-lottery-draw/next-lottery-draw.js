eaae36f9a2aa3dfe4734323ca79fb8fa63984afe
adelia ferreira <tay.figueira@gmail.com>
2020-11-20 23:54:50 +0000

    Added validate to time after draw and saturday
    Signed-off-by: adelia ferreira <tay.figueira@gmail.com>

--- next-irish-lottery-draw/next-lottery-draw.js
+++ next-irish-lottery-draw/next-lottery-draw.js
@@ -1,18 +1,33 @@
-function getNextIrishLotteryDraw(today) {
-    const diffDaysOfWeeksToNextDraw = new Map([
-      [0, 3],
-      [1, 2],
-      [2, 1],
-      [3, 0],
-      [4, 2],
-      [5, 1],
-      [6, 0]
-    ]);
-    const nextDraw = new Date();
-    nextDraw.setDate(
-      today.getDate() + diffDaysOfWeeksToNextDraw.get(today.getDay())
-    );
-    return nextDraw;
+function getNextIrishLotteryDraw(currentDate = new Date()) {
+  const diffDaysOfWeeksToNextDraw = new Map([
+    [0, 3],
+    [1, 2],
+    [2, 1],
+    [3, 0],
+    [4, 2],
+    [5, 1],
+    [6, 0]
+  ]);
+  let nextDrawDay =
+    currentDate.getDate() + diffDaysOfWeeksToNextDraw.get(currentDate.getDay());
+  const nextDraw = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    nextDrawDay,
+    19,
+    55,
+    0,
+    0
+  );
+  if (currentDate > nextDraw) {
+    let nextDayIndex = currentDate.getDay();
+    nextDayIndex = nextDayIndex === 6 ? 0 : nextDayIndex + 1;
+    nextDrawDay =
+      currentDate.getDate() + diffDaysOfWeeksToNextDraw.get(nextDayIndex);
+      nextDrawDay += 1;
+    nextDraw.setDate(nextDrawDay);
   }
+  return nextDraw;
+}
 //   const today = new Date();
 //   console.log(getNextIrishLotteryDraw(today));
